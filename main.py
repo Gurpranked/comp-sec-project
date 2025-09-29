2b746cdbcf876eee35cfa8a91737a2a8e0bb7d0e
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-09-29 10:11:26 -0400

    feat: Implemented sha256 password hashing with salting and storage to yaml record keeping

--- main.py
+++ main.py
@@ -2,14 +2,18 @@
 # Author: Gurpreet Singh
 from user_registration import user_registration
 
+CREDS_FILENAME = "creds.yml"
+
 def driver():
     accounts_exist = False
     if not accounts_exist:
+        print("No users are registered on this client.")
         register = input("Do you want to register a new user (y/n)?")
+        print()
         if (register == "y"):
-            user_registration()
+            user_registration(CREDS_FILENAME)
             print("Exiting SecureDrop.")
-            exit(1)
+            exit(0)
 
 
 if __name__ == "__main__":
--- user_registration.py
+++ user_registration.py
@@ -2,18 +2,47 @@
 # Author: Gurpreet Singh
 from getpass import getpass
 from bcolors import bcolors
+import hashlib
+import yaml
+import os
 
-def user_registration():
+PEPPER = "somegoodpeppa"
+
+def user_registration(creds_filename: str):
     full_name = input(f"{bcolors.BOLD}Enter Full Name: {bcolors.ENDC}")
     email = input(f"{bcolors.BOLD}Enter Email Address: {bcolors.ENDC}")
-    password = ""
-    password1 = "1"
-    while not (password == password1):
-        password = getpass(f"{bcolors.BOLD}Enter Password: {bcolors.ENDC}") 
-        password1 = getpass(f"{bcolors.BOLD}Re-enter Password: {bcolors.ENDC}")
-        if not (password == password1) :
+    pwd = ""
+    pwd1 = "1"
+    while not (pwd == pwd1):
+        pwd = getpass(f"{bcolors.BOLD}Enter Password: {bcolors.ENDC}") 
+        pwd1 = getpass(f"{bcolors.BOLD}Re-enter Password: {bcolors.ENDC}")
+        if not (pwd == pwd1):
             print(f"{bcolors.FAIL}Passwords don't match, try again{bcolors.ENDC}")
+     
+    print(f"\n{bcolors.OKGREEN}Passwords match.{bcolors.ENDC}")    
 
-    print(f"{bcolors.OKGREEN}Passwords match.{bcolors.ENDC}")    
-
+    # Hashing the password
+    pwd_hash = create_secure_pwd(pwd)
+    salt, key = pwd_hash[:16], pwd_hash[16:]
+    hash_algo = "sha256"
+    iterations = 100_000
     # Something something user registered
+    data = {
+        f'{email}': {
+            'name': f'{full_name}',
+            'pwd_hash': f'{key}',
+            'salt': f'{salt}',
+            'hash_algo': f'{hash_algo}',
+        }
+    }
+
+    with open(creds_filename, 'w+') as file:
+        yaml.dump(data, file)
+
+    print("User Registered.")
+
+def create_secure_pwd(pwd):
+    salt = os.urandom(16)
+    pwd_salt = b'pwd' + salt
+    pwd_hash = hashlib.sha256(pwd_salt)
+    return pwd_hash.hexdigest()
